9b7d03af1f3623704e9483618c6e6709784bf504
Tomas valentinis <valentinistomas@gmail.com>
2024-02-22 23:26:02 +0100

    primer movimiento y limites

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,19 +1,56 @@
-extends StaticBody2D
+extends CharacterBody2D
 
-var win_height : int
-var p_height : int
+# Velocidad de movimiento
+@export var speed: float = 200
+
+# Límites de movimiento
+var min_x: float = 0
+var max_x: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	win_height = get_viewport_rect().size.y
-	p_height = $ColorRect.get_size().y
+	# Obtener los límites de movimiento a partir de las colisiones de los nodos hijos
+	for child in get_children():
+		if child.has_method("get_collision_box"):
+			var collision_box = child.get_collision_box()
+			min_x = collision_box.position.x - collision_box.size.x / 2
+			max_x = collision_box.position.x + collision_box.size.x / 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_pressed("ui_up"):
-		position.y -= get_parent().PADDLE_SPEED * delta
-	elif Input.is_action_pressed("ui_down"):
-		position.y += get_parent().PADDLE_SPEED * delta
+func _physics_process(_delta):
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity += direction * 50
+	move_and_slide() 
+	# Movimiento horizontal
+	#var move_dir = 0
+	#if Input.is_action_pressed("ui_left"):
+		#move_dir -= 1
+	#if Input.is_action_pressed("ui_right"):
+		#move_dir += 1
+	#
+	#var velocity = Vector2(move_dir * speed * delta, 0)
+	#move_and_slide(velocity)
+#
+	## Limitar el movimiento dentro de los límites obtenidos
+	#position.x = clamp(position.x, min_x, max_x)
 
-	#limit paddle movement to window
-	position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
+
+#extends StaticBody2D
+#
+#var win_height : int
+#var p_height : int
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#win_height = get_viewport_rect().size.y
+	#print($CollisionShape2D.shape)
+	#p_height = $CollisionShape2D.shape.radius
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#if Input.is_action_pressed("ui_left"):
+		#position.y -= get_parent().PADDLE_SPEED * delta
+	#elif Input.is_action_pressed("ui_right"):
+		#position.y += get_parent().PADDLE_SPEED * delta
+#
+	##limit paddle movement to window
+	#position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
